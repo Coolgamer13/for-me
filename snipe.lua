21e67463185bf9fae0e054c9614e680eae07264b
Coolgamer13 <106827435+Coolgamer13@users.noreply.github.com>
2024-01-02 18:49:20 +0200

    Update snipe.lua

--- snipe.lua
+++ snipe.lua
@@ -146,6 +146,9 @@ local function checklisting(uid, gems, item, version, shiny, amount, username, p
             ping = true
         end
         processListingInfo(uid, gems, item, version, shiny, amount, username, boughtPet, ping)
+    elseif type.exclusiveLevel and price <= 10000 and item ~= "Banana" and item ~= "Coin" then
+        local boughtPet, boughtMessage = purchase:InvokeServer(playerid, uid)
+        processListingInfo(uid, gems, item, version, shiny, amount, username, boughtPet, ping)
     elseif string.find(item, "Exclusive") and price <= 25000 then
         local boughtPet, boughtMessage = purchase:InvokeServer(playerid, uid)
         processListingInfo(uid, gems, item, version, shiny, amount, username, boughtPet, ping)
@@ -176,9 +179,9 @@ local function checklisting(uid, gems, item, version, shiny, amount, username, p
     elseif item == "Huge Hunter" and gems <= 100000 then
         local boughtPet, boughtMessage = purchase:InvokeServer(playerid, uid)
         processListingInfo(uid, gems, item, version, shiny, amount, username, boughtPet, ping)
-    elseif string.find(item, "Superior") and price <= 100000 then
-        local boughtPet, boughtMessage = purchase:InvokeServer(playerid, uid)
-        processListingInfo(uid, gems, item, version, shiny, amount, username, boughtPet, ping) 
+    elseif type.superiorLevel and price <= 100000 and item ~= "Exotic Treasure Flag" then
+    local boughtPet, boughtMessage = purchase:InvokeServer(playerid, uid)
+    processListingInfo(uid, gems, item, version, shiny, amount, username, boughtPet, ping)  
     end
 end
 
